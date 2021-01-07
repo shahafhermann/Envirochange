0dbe196eb76dc697fba6f529dffab69037025718
nadav.alali <nadav.alali@mail.huji.ac.il>
2021-01-07 13:13:55 +0200

    some updates
    added option to multi mid air jump
    added movement on ground and in air

--- Assets/Scripts/CreatureBehaviour.cs
+++ Assets/Scripts/CreatureBehaviour.cs
@@ -10,33 +10,54 @@ public class CreatureBehaviour : MonoBehaviour
     private Rigidbody2D creature_rigid;
     public float jumpHeight = 0f;
     private bool isJumping = false;
-    [FormerlySerializedAs("midAirTurnForce")] public float sidewaySpeed = 5f;
+    public float groundSpeed = 5f;
+    public float midAir = 5f;
+    public int MAX_JUMPS_ROW = 2;
+    private int num_of_jumps;
 
     
     // Start is called before the first frame update
     void Start()
     {
         creature_rigid = GetComponent<Rigidbody2D>();
+        num_of_jumps = MAX_JUMPS_ROW;
+    }
+
+    private void Update()
+    {
+        if (num_of_jumps == MAX_JUMPS_ROW)
+        {
+            if (Input.GetKey("left"))
+            {
+                creature_rigid.transform.position += Vector3.left * groundSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey("right"))
+            {
+                creature_rigid.transform.position += Vector3.right * groundSpeed * Time.deltaTime;
+            }
+        }
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        
-        if (Input.GetKey("space") && !isJumping)
+        Debug.Log("jumps = " + num_of_jumps);
+        if (num_of_jumps  < MAX_JUMPS_ROW)
+        {
+            if (Input.GetKey("left"))
+            {
+                creature_rigid.AddForce(new Vector2(-midAir, 0));
+            }
+            if (Input.GetKey("right"))
+            {
+                creature_rigid.AddForce(new Vector2(midAir, 0));
+            }
+        }
+        if (Input.GetKeyDown("space") && num_of_jumps > 0)
         {
             creature_rigid.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
-            isJumping = true;
-        }
-        
-        if (Input.GetKey("left"))
-        {
-            creature_rigid.AddForce(new Vector2(-sidewaySpeed, 0));
-        }
-        if (Input.GetKey("right"))
-        {
-            creature_rigid.AddForce(new Vector2(sidewaySpeed, 0));
+            num_of_jumps--;
         }
         
     }
@@ -46,7 +67,7 @@ public class CreatureBehaviour : MonoBehaviour
         if (other.gameObject.tag == "Platform")
         {
             Debug.Log("made contact!");
-            isJumping = false;
+            num_of_jumps = MAX_JUMPS_ROW;
         }
     }
 }
