6bffc507dd0a2284b1086bb1218f442d9482c236
nadav.alali <nadav.alali@mail.huji.ac.il>
2021-01-16 14:54:37 +0200

    fixed children collider bug

--- Assets/Scripts/PlatformBehaviour.cs
+++ Assets/Scripts/PlatformBehaviour.cs
@@ -1,24 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlatformBehaviour : MonoBehaviour
 {
+    private Collider2D[] platformColliders;
+    public void Start()
+    {
+        platformColliders = gameObject.GetComponentsInChildren<Collider2D>();
+
+
+    }
+
     public void AlertObservers(string message)
     {
         if (message.Equals("AppearAnimationEnded")) {
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
-            gameObject.GetComponentInChildren<BoxCollider2D>().enabled = true;
-            if (gameObject.CompareTag("Magnet")) {
-                gameObject.GetComponentInChildren<CapsuleCollider2D>().enabled = true;
-            }
+            // gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            // gameObject.GetComponentInChildren<BoxCollider2D>().enabled = true;
+            // if (gameObject.CompareTag("Magnet")) {
+            //     gameObject.GetComponentInChildren<CapsuleCollider2D>().enabled = true;
+            // }
+            for (var index = 0; index < platformColliders.Length; index++)
+            {
+                platformColliders[index].enabled = true;
+            } 
         } 
         if (message.Equals("DisappearAnimationStarted")) {
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            gameObject.GetComponentInChildren<BoxCollider2D>().enabled = false;
-            if (gameObject.CompareTag("Magnet")) {
-                StartCoroutine(disableGravityField());
-            }
+            // gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            // gameObject.GetComponentInChildren<BoxCollider2D>().enabled = false;
+            // if (gameObject.CompareTag("Magnet")) {
+            //     StartCoroutine(disableGravityField());
+            // }
+            for (var index = 0; index < platformColliders.Length; index++)
+            {
+                platformColliders[index].enabled = false;
+            } 
         }
     }
 
