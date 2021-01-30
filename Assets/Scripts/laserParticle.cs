82bbc519b7158339f37806051106de7affdd1fd0
nadav.alali <nadav.alali@mail.huji.ac.il>
2021-01-30 22:58:18 +0200

    fixed static laser loop

--- Assets/Scripts/laserParticle.cs
+++ Assets/Scripts/laserParticle.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class laserParticle : MonoBehaviour
 {
+    public bool should_loop = false;
     private float _time;
 
     private ParticleSystem particles;
@@ -12,18 +14,25 @@ public class laserParticle : MonoBehaviour
     {
         particles = GetComponent<ParticleSystem>();
         _time = 0f;
+        if (should_loop)
+        {
+            particles.loop = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _time += Time.deltaTime;
-        if (_time >= 2 * particles.duration)
+        if (!should_loop)
         {
-            Debug.Log(_time);
-            particles.Stop();
-            particles.Play();
-            _time = 0f;
+            _time += Time.deltaTime;
+            if (_time >= 2 * particles.duration)
+            {
+                Debug.Log(_time);
+                particles.Stop();
+                particles.Play();
+                _time = 0f;
+            }
         }
     }
 }
