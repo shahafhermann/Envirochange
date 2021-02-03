b7e297bceaaa8732dbfac578ba611362ff61d08d
nadav.alali <nadav.alali@mail.huji.ac.il>
2021-02-03 13:48:18 +0200

    added offset option for shake

--- Assets/Scripts/Shakeit.cs
+++ Assets/Scripts/Shakeit.cs
@@ -9,6 +9,8 @@ public class Shakeit : MonoBehaviour
     
     [Range(0f, 200f)]
     public float max_speed = 85.0f;
+
+    [Range(0f, 0.1f)] public float max_offset = 0.062f;
     
     private float speed; //how fast it shakes
     private Vector3 original_position;
@@ -24,8 +26,8 @@ public class Shakeit : MonoBehaviour
     void Update()
     {
         float noise = Mathf.Sin(Time.time * speed);
-        float x_pos = original_position.x + (noise * Random.Range(-0.062f, .062f));
-        float y_pos = original_position.y + (noise * Random.Range(-0.062f, .062f));
+        float x_pos = original_position.x + (noise * Random.Range(-max_offset, max_offset));
+        float y_pos = original_position.y + (noise * Random.Range(-max_offset, max_offset));
         float z_pos = original_position.z;
         transform.position = new Vector3(x_pos, y_pos, z_pos);
     }
